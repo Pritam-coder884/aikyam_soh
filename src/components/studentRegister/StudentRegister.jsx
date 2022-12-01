1ed37ddccb3487af292734904bed25ed232b1209
Pritam kumar dash <pritamkumar72002@gmail.com>
2022-12-01 05:38:50 +0530

    backend integration commit

--- src/components/studentRegister/StudentRegister.jsx
+++ src/components/studentRegister/StudentRegister.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../../pages/Login/Login.css";
-import { createStudent } from "../../utils/api/api.utils";
 import axios from "axios";
 
 const StudentRegister = () => {
